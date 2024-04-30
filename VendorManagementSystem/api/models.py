fddbd7d96fd44c9389e3abd1fcf744526a1976c3
Kushagra Mahalwal <98391378+KushagraMahalwal@users.noreply.github.com>
2024-04-30 16:18:46 +0000

    code update

--- VendorManagementSystem/api/models.py
+++ VendorManagementSystem/api/models.py
@@ -11,18 +11,17 @@ class Vendor(models.Model):
         return self.name
 
 class PurchaseOrder(models.Model):
-    # Fields
     po_number = models.CharField(max_length=50, unique=True, blank=True, null=True, editable=False)
     vendor_reference = models.CharField(max_length=100)
     order_date = models.DateField(auto_now_add=True)
-    items = models.TextField()  # You might consider a more specific field type depending on your data structure
+    items = models.TextField()
     quantity = models.IntegerField()
     status = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         # Generate a unique PO number using UUID
         if not self.po_number:
-            self.po_number = str(uuid.uuid4())[:8].upper()  # You can adjust the length and formatting as needed
+            self.po_number = str(uuid.uuid4())[:8].upper()
         super().save(*args, **kwargs)
 
     def __str__(self):
